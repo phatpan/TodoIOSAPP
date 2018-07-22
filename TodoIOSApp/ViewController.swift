e4923f15f9387d773a4c40d6bdb8837de2bf2482
phatcharaphan <phat.pan@gmail.com>
2018-07-22 11:43:11 +0700

    add accessoryType

--- TodoIOSApp/ViewController.swift
+++ TodoIOSApp/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoItemCell", for: indexPath)
         cell.textLabel?.text = todo.item(at: indexPath.row).title
+        cell.accessoryType = todo.item(at: indexPath.row).isDone ? .checkmark : .none
         return cell
     }
     
@@ -24,7 +25,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         todo.add(item: TodoItem(title: "Angular"))
         todo.add(item: TodoItem(title: "React"))
-        todo.add(item: TodoItem(title: "Vue"))
+        todo.add(item: TodoItem(title: "Vue", isDone: true))
     }
 
 }
