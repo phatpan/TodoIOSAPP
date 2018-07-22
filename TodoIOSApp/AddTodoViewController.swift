cfd963ca63e8c5a825df7dfe2426cac3d032bd7f
phatcharaphan <phat.pan@gmail.com>
2018-07-22 12:08:10 +0700

    add didmiss button

--- TodoIOSApp/AddTodoViewController.swift
+++ TodoIOSApp/AddTodoViewController.swift
@@ -12,24 +12,13 @@ class AddTodoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cancelButtonDidtap(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneButtonDidtab() {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
 }
