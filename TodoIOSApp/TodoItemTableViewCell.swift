abdf792b4fbab9783d69e3346d17a19e2ca18563
phatcharaphan <phat.pan@gmail.com>
2018-07-22 15:11:21 +0700

    new file todo item table view cell

---
+++ TodoIOSApp/TodoItemTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TodoItemTableViewCell.swift
+//  TodoIOSApp
+//
+//  Created by Phatcharaphan Ananpreechakun on 7/22/18.
+//  Copyright © 2018 Phatcharaphan Ananpreechakun. All rights reserved.
+//
+
+import UIKit
+
+class TodoItemTableViewCell: UITableViewCell {
+}
