030ca0217ac11cb321f0f2540b5f12265b34dccd
phatcharaphan <phat.pan@gmail.com>
2018-07-22 11:23:00 +0700

    add class todo

---
+++ TodoIOSApp/Todo.swift
@@ -0,0 +1,46 @@
+//
+//  Todo.swift
+//  TodoIOSApp
+//
+//  Created by Phatcharaphan Ananpreechakun on 7/22/18.
+//  Copyright © 2018 Phatcharaphan Ananpreechakun. All rights reserved.
+//
+
+import Foundation
+
+class Todo {
+    private var items = [TodoItem]()
+    var totalItem: Int {
+        return items.count
+    }
+
+    func add(item: TodoItem) {
+        items.insert(item, at: 0)
+    }
+
+    func remove(index: Int){
+        items.remove(at: index)
+    }
+
+    func item(at index: Int)  -> TodoItem {
+        return items[index]
+    }
+
+    func index(of item: TodoItem) -> Int? {
+        return items.index { $0 === item }
+    }
+}
+
+class TodoItem {
+    var title: String
+    var isDone: Bool
+
+    init(title: String, isDone: Bool) {
+        self.title = title
+        self.isDone = isDone
+    }
+
+    func toggleIsDone() {
+        isDone = !isDone
+    }
+}
--- TodoIOSApp/ViewController.swift
+++ TodoIOSApp/ViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
